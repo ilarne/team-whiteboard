c438c4b72b3c15847bee9158c4153f3123e6b9d6
Timothy Robinson <timmyrobby@gmail.com>
2017-06-19 11:55:11 +0100

    Adds test for click array

--- src/whiteboard.js
+++ src/whiteboard.js
@@ -1,3 +1,9 @@
 function Whiteboard() {
-  
+  this.painting = false;
+  this.clickX = new Array();
+  this.clickY = new Array();
+  this.clickDrag = new Array();
+  this.colour = "#000000"
 }
+
+module.exports = Whiteboard;
--- test/whiteboardSpec.js
+++ test/whiteboardSpec.js
@@ -3,7 +3,17 @@ const expect = chai.expect;
 const Whiteboard = require('../src/whiteboard.js');
 
 describe('Whiteboard', function() {
+  var whiteboard;
+
+  beforeEach(function() {
+    whiteboard = new Whiteboard();
+  })
+
   it('should be defined', function() {
     expect(Whiteboard).isDefined;
+  })
+
+  it('starts with an empty array of clicks', function() {
+    expect(whiteboard.clickX).empty;
   })
 })
