b84a8e8dc8d5a4a352cc2d80dc6814dd8d4c8b8f
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2017-06-20 11:55:05 +0100

    adds test for multiple socket connections

--- index.js
+++ index.js
@@ -12,29 +12,14 @@ app.get('/', function (req, res) {
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('connection name', function(user){
+    io.sockets.emit('new user', user.name + " has joined.") 
+  })
   socket.on('disconnect', function(){
     console.log('user disconnected');
-  });
-});
+  })
+})
 
 http.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
-
-
-
-// var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-//
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
-//
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-// });
-//
-// http.listen(3000, function(){
-//   console.log('listening on *:3000');
-// });
