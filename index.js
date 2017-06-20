0f546b0f1f98dd18847faf68553c29d78f87d187
Timothy Robinson <timmyrobby@gmail.com>
2017-06-20 12:04:53 +0100

    Reads information successfully from local MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const express = require('express')
+var router = express.Router();
 const app = express()
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/testDB');
+
+app.engine('.html', require('ejs').renderFile);
+
+// Make our db accessible to our router
+app.use(function(req, res, next){
+  req.db = db;
+  next();
+});
+
+/* GET Userlist page. */
+app.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e, docs){}).then( function(info) {
+    res.render('testPage.html', {
+      bailey: info[0].name,
+      tim: info[1].name
+    });
+  })
+});
 
 app.use(express.static('public'))
 
