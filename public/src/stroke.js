b5745ebd42a38b02099353271718582e2b4bf406
isabellarner <larner.isabel@gmail.com>
2017-06-21 17:06:43 +0100

    adds addClick function to Stroke

--- public/src/stroke.js
+++ public/src/stroke.js
@@ -4,4 +4,10 @@ function Stroke() {
   this.clickDrag = []
 }
 
+Stroke.prototype.addClick = function(x, y, drag) {
+  this.clickX.push(x)
+  this.clickY.push(y)
+  this.clickDrag.push(drag)
+}
+
 module.exports = Stroke
--- test/strokeSpec.js
+++ test/strokeSpec.js
@@ -24,4 +24,21 @@ describe('Initialisation', function(){
   })
 })
 
+  describe('#addClick', function() {
+    it('adds clicks to the x-axis', function() {
+      stroke.addClick(1);
+      expect(stroke.clickX[0]).equal(1);
+    })
+
+    it('adds clicks to the y-axis', function() {
+      stroke.addClick(1, 2);
+      expect(stroke.clickY[0]).equal(2);
+    })
+
+    it('records whether it is a dragged click', function() {
+      stroke.addClick(1, 2, true);
+      expect(stroke.clickDrag[0]).equal(true);
+    })
+  })
+
 })
