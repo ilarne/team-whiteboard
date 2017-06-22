61ae5c29cfc97b90b6218a97b97783b1b54fc361
isabellarner <larner.isabel@gmail.com>
2017-06-22 15:55:06 +0100

    adds event listenenr to change cursor image when different colour is picked

--- public/src/paletteEventListeners.js
+++ public/src/paletteEventListeners.js
@@ -4,8 +4,14 @@ $(".colour-button").click(function() {
 
 document.addEventListener("DOMContentLoaded", function () {
   var colours = document.querySelectorAll(".colour-button");
+  var body = document.getElementsByTagName("body")[0];
 
   colours.forEach(function(colour) {
     colour.style.backgroundColor = colour.getAttribute('data-value');
+    colour.addEventListener('click', function() {
+      body.style.cursor = 'url(/images/cursors/' + colour.id + 'marker.png), pointer';
+    })
   })
+
+
 })
