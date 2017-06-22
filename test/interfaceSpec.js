4b8d25c8235c0bb9c8cd8b228df26f1b5a36291a
isabellarner <larner.isabel@gmail.com>
2017-06-22 18:19:09 +0100

    kills zombie (interfaceSpec.js) - not needed right now

--- test/interfaceSpec.js
+++
@@ -1,8 +0,0 @@
-const Browser = require('zombie');
-const assert = require('assert');
-const url = 'http://localhost:3000/';
-const browser = new Browser();
-
-before(function(done){
-  return browser.visit(url, done);
-});
