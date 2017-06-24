5f43ecab5febe18146c9c2935eff590f6b298f03
Bailey Kursar <bkursar@gmail.com>
2017-06-24 18:20:51 +0100

    Added files for postits and tests

---
+++ test/whiteboard/postitSpec.js
@@ -0,0 +1,9 @@
+describe('Postit', function() {
+  // beforeEach(function() {
+  //   postit = new Postit();
+  // })
+
+  // it('initialises', function() {
+  //   assert.exists(postit);
+  // })
+})
