fd1f42f6a3654d33dc37d676a604f077bb76f4c6
Timothy Robinson <timmyrobby@gmail.com>
2017-06-26 17:16:13 +0100

    Creates test objects for zombie to use

---
+++ test/interfaceSpec.js
@@ -0,0 +1,14 @@
+const Browser = require('zombie');
+const assert = require('assert');
+const url = 'http://localhost:3000/';
+const browser = new Browser();
+
+before(function(done) {
+  return browser.visit(url, done);
+});
+
+describe('User visits page', function() {
+  it('displays test text', function() {
+    assert.equal(browser.text('h1'), "Aaaaaah it's working!");
+  })
+})
