dc8a96e1d183d5d2bb2bd5bb9c1cee68b53b3b5b
Timothy Robinson <timmyrobby@gmail.com>
2017-06-26 20:34:52 +0100

    User account feature testing

--- userSpec.js
+++ userSpec.js
@@ -13,13 +13,38 @@ fixture `User Account Management`
     .expect(Selector('#user').innerText).eql('');
   });
 
-  test('User name is recorded succesfully', async time => {
+  test('Sign-up prompt displays when not logged in', async time => {
+    await time
+    .expect(Selector('.user-message').innerText).eql('Sign up to Onbord!');
+  });
+
+  test('User name is recorded succesfully when signing in', async time => {
     await time
       .click('#signup-button')
-      .typeText('#signup-name', 'bark')
-      .typeText('#signup-username', 'bark')
-      .typeText('#signup-email', 'bark@gmail.com')
-      .typeText('#signup-password', 'bark')
+      .typeText('#signup-name', 'John Smith')
+      .typeText('#signup-username', 'JSmith')
+      .typeText('#signup-email', 'smith@gmail.com')
+      .typeText('#signup-password', '123')
       .click('#create-account')
-      .expect(Selector('#user').innerText).eql('bark');
+      .expect(Selector('#user').innerText).eql('JSmith');
+  });
+
+  test('Page confirms that the user is signed in', async time => {
+    await time
+      .click('#signup-button')
+      .typeText('#signup-name', 'Test User')
+      .typeText('#signup-username', 'TestUser')
+      .typeText('#signup-email', 'testuser@gmail.com')
+      .typeText('#signup-password', '123')
+      .click('#create-account')
+      .expect(Selector('.user-message').innerText).eql('Logged in as TestUser');
+  });
+
+  test('Can log in to an existing account', async time => {
+    await time
+      .click('#login-button')
+      .typeText('#login-username', 'JSmith')
+      .typeText('#login-password', '123')
+      .click('#login-submit')
+      .expect(Selector('.user-message').innerText).eql('Logged in as JSmith');
   });
